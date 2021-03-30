b542b76431fa3e9cf544b5102ef3cbeebc39e69c
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-03-30 00:03:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {return 'This one is on me!';}
+  if (feet > 2500) return 'No can do.';
+  if (feet > 2000) {return 'I will gladly take your thirty bucks.';}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const msg = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return msg;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
